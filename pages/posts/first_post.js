685e0f82a48bfaa9bd89908040d3872ac5161064
howardyangyixuan <howardyangyixuan@gmail.com>
2021-07-11 09:41:44 +0800

    使用module添加局部css

--- pages/posts/first_post.js
+++ pages/posts/first_post.js
@@ -1,11 +1,15 @@
 import React from "react"
 import Link from "next/link"
 import Head from "next/head"
+import styles from "styles/FirstPost.module.css"
 
 export default function FirstPost() {
   return (
     <React.Fragment>
       <div>First Page
+        <div className={styles.wrapper}>
+          <div className={styles.content}>hi</div>
+        </div>
         <hr/>
         <Link href="/"><a>回到首页</a></Link>
       </div>
