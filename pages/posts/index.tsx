a49272fedf0c2a935c66d20ed7abd6aef6c78dbb
howardyangyixuan <howardyangyixuan@gmail.com>
2021-07-12 12:47:49 +0800

    使用axios实现客户端渲染

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,10 +1,39 @@
 import {NextPage} from 'next';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
+type Post = {
+  id: string;
+  date: string;
+  title: string;
+}
 const PostsIndex: NextPage = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isEmpty, setIsEmpty] = useState<boolean>(false);
+  useEffect(() => {
+    setIsLoading(true);
+    axios.get('api/v1/posts').then((response) => {
+      setPosts(response.data);
+      setIsLoading(false);
+      if (response.data.length == 0) setIsEmpty(true);
+    }, () => {
+      setIsLoading(false);
+    });
+  }, []);
   return (
     <div>
-      Posts Index
+      <h1>文章列表</h1>
+      {isLoading ? <div>正在加载中</div> :
+        isEmpty ? <div>正在构思中...目前还有没有文章</div> :
+          <ul>
+            {posts.map((post) => {
+              return (
+                <li key={post.id}>{post.title}</li>
+              );
+            })}
+          </ul>
+      }
     </div>
   );
 };
