70a84aeb32225bc0dcaeab0291f9e89d9ddc0dfa
howardyangyixuan <howardyangyixuan@gmail.com>
2021-07-23 00:34:28 +0800

    完成文章详情css，支持markdown，使用marked和github-markdown-css

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {NextPage, NextPageContext} from 'next';
 import {getPost} from '../../lib/posts';
 import {Post} from '../../src/entity/Post';
+import marked from 'marked'
 
 type Props = {
   post: Post
@@ -9,12 +10,21 @@ type Props = {
 const Page: NextPage<Props> = (props) => {
   const {post} = props;
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <article>
-        {post.content}
-      </article>
-    </div>
+    <>
+      <div className="wrapper">
+        <h1>{post.title}</h1>
+        <article className="markdown-body" dangerouslySetInnerHTML={{__html: marked(post.content)}}>
+        </article>
+      </div>
+      <style jsx>{`
+      .wrapper{
+        max-width: 800px;
+        margin: 16px auto;
+        padding: 0 16px;
+      }
+      h1{padding-bottom: 16px; border-bottom: 1px solid #666;}
+      `}</style>
+    </>
   );
 };
 export default Page;
