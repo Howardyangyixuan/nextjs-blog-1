8188e77f97796f444c821d64dcd0ae54b633960a
howardyangyixuan <howardyangyixuan@gmail.com>
2021-07-22 11:58:22 +0800

    清除调试信息，修改perPage为10

--- lib/users.tsx
+++ lib/users.tsx
@@ -3,9 +3,7 @@ import {User} from '../src/entity/User';
 
 export const addUser = async (user: User) => {
   const connection = await getDatabaseConnection();
-  let users = await connection.manager.insert('users', user);
-  console.log(users);
-  console.log('----------');
+  await connection.manager.insert('users', user);
 };
 export const findUser = async (username: string) => {
   const connection = await getDatabaseConnection();
